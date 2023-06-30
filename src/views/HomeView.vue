666cd5b03da5b7b4bb57041be513a9bdea764a82
MQuintanaV <maritza.quintana.vasquez@gmail.com>
2023-06-30 15:04:59 -0400

    creacion del repositorio local

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <h1>Buscador</h1>  
+      <label for="txtPokemon">ingrese Id pokemon (1-200)</label>
+      <input type="number" id="txtPokemon" v-model="pokemon_id">      
+      <button v-on:click="search">buscar</button>
+      
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
+  },
+  data: function(){
+    return{
+      pokemon_id: 0,
+    }
+  },
+  methods:{
+    search:function(){
+        // evaluamos si la variable esta vacia para retornar el control al form
+        // es decir, finaliza el trabajo de el metodo search
+        if(this.pokemon_id === "") 
+          return
+        this.$router.push(`/pokemon/${this.pokemon_id}`);
+    },
   }
 }
 </script>
+
+<style scoped>
+  form{
+    border:2px solid #42b983;
+    padding-bottom: 20px;
+    width: 60%;
+    margin: 0 auto;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports ={
+    publicpath: process.env.NODE_ENV === 'produccion' ? '././' : './',
+};
